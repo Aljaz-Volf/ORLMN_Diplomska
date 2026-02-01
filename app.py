2c9802838d77793331b3a016855996eac9629372
Aljaz-Volf <aljaz.wolf@gmail.com>
2026-02-01 14:33:44 +0100

    Update app.py
    Sprememba za render acces

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -13,5 +14,5 @@ def home():
     return render_template("home.html", stats=stats)
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
